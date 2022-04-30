37e5f25aea89174a89646c6230f8c024aaff3834
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2022-04-30 18:27:51 +0530

    Update profile-version-1.sh

--- profile-version-1.sh
+++ profile-version-1.sh
@@ -12,11 +12,18 @@ target_dir=""
 
 
 
-ssh_key_loc="/root/.ssh/id_rsa"
+ssh_key_loc="/root/.ssh/id_rsa_wp_backup_profile_hetbackup"
 date=$(date +"%d-%b-%Y")
 wpmysqluser=$(grep -w "DB_NAME" $target_dir/wp-config.php | cut --delimiter="'"  --fields="4")
 wpmysqldb=$(grep -w "DB_USER" $target_dir/wp-config.php | cut --delimiter="'"  --fields="4")
 wpmysqluserpass=$(grep -w "DB_PASSWORD" $target_dir/wp-config.php | cut --delimiter="'"  --fields="4")
+
+
+# create backup folder if not exist
+
+echo "Creating backup directory"
+mkdir -p $target_dir/wpbackups
+echo "Finished creating directory"
 
 
 #Create database dump file or backup file.
@@ -25,7 +32,7 @@ echo "....................."
 echo "Exporting sql file..."
 echo "....................."
 echo "....................."
-mysqldump --no-tablespaces -u $wpmysqluser -p$wpmysqluserpass $wpmysqldb > $target_dir/$date.sql
+mysqldump --no-tablespaces -u $wpmysqluser -p$wpmysqluserpass $wpmysqldb > $target_dir/wpbackups/$date.sql
 echo "done.!"
 echo "....................."
 echo "....................."
@@ -35,8 +42,9 @@ echo "This SQL file will be sent to remote location via rsync"
 
 echo "....................."
 echo "....................."
-echo "starting rsync backup..."
-rsync --progress -auvz -e "ssh -p23 -i $ssh_key_loc" --recursive $target_dir $remote_user@$remote_server:$remote_dirs/$date > /wp-backups-profile/$date.txt
+echo "starting rsync backup"
+echo "This process will take time depending on size of your website..."
+rsync --progress -auvz -e "ssh -p23 -i $ssh_key_loc" --recursive $target_dir $remote_user@$remote_server:$remote_dirs/$date > $target_dir/wpbackups/rsync_log.txt
 echo "done.!"
 echo "....................."
 echo "....................."
@@ -48,7 +56,7 @@ echo "Finished sending backup date directory to remote location"
 echo "deleting your SQL file to avoid impacting disk space usage"
 echo "....................."
 echo "....................."
-rm $target_dir/$date.sql
+rm $target_dir/wpbackups/$date.sql
 echo "done.!"
 echo "....................."
 echo "....................."
