ea9395e88dd02a1408b70867eaaf35fd15bdf714
Manj (WPzonify) <81701675+manoj-wpzonify@users.noreply.github.com>
2022-05-01 17:58:57 +0530

    Update profile-compressed-backup.sh

--- profile-compressed-backup.sh
+++ profile-compressed-backup.sh
@@ -21,30 +21,30 @@ wpmysqluserpass=$(grep -w "DB_PASSWORD" $target_dir/wp-config.php | cut --delimi
 # create backup folder if not exist
 
 echo "Creating backup directory"
-mkdir -p $target_dir/wp-backup-profile/$date
+mkdir -p $target_dir/backuplift/$date
 echo "Finished creating directory"
 
 #Create database dump file or backup file.
 
-mysqldump --no-tablespaces -u $wpmysqluser -p$wpmysqluserpass $wpmysqldb > $target_dir/wp-backup-profile/$date/database-$date-$random_file_name.sql
+mysqldump --no-tablespaces -u $wpmysqluser -p$wpmysqluserpass $wpmysqldb > $target_dir/backuplift/$date/database-$date-$random_file_name.sql
 
 #Creating directory zip file with date.
 
 echo "Compressing your directory"
-tar -zcvf $target_dir/wp-backup-profile/$date/files-$date-$random_file_name.tar.gz $target_dir 
+tar -zcvf $target_dir/backuplift/$date/files-$date-$random_file_name.tar.gz $target_dir 
 echo "compressed the directory is finished"
 
 #transferring compressed date directory via rsync
 
 echo
 echo "starting rsync backup..."
-rsync --progress -auvz -e 'ssh -p23 $ssh_connect' --recursive $target_dir/wp-backup-profile/$date $remote_user@$remote_server:$remote_dirs
+rsync --progress -auvz -e 'ssh -p23 $ssh_key_loc' --recursive $target_dir/backuplift/$date $remote_user@$remote_server:$remote_dirs
 echo "done. Finished sending backup date directory to remote location"
 
 #delete backup folder after transfering to remote location.
 
 echo "deleting your backup date folder & sql file"
-rm -rf $target_dir/wp-backup-profile
+rm -rf $target_dir/backuplift
 echo "finished deleting the backup"
 echo "....................."
 echo "....................."
